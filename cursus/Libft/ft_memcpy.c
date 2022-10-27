a00eb27aa138cbe5d830745596de7f628480ab36
egdit <105239564+egdit@users.noreply.github.com>
2022-10-27 22:07:46 +0300

    Update ft_memcpy.c

--- cursus/Libft/ft_memcpy.c
+++ cursus/Libft/ft_memcpy.c
@@ -14,15 +14,16 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char	*source;
-	char	*dest;
+	size_t	i;
 
-	source = (char *)src;
-	dest = (char *)dst;
-	if (!dst && !src)
-		return (dst);
-	while (n--)
-		*dest++ = *source++;
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		*(char*)(dst + i) = *(char*)(src + i);
+		i++;
+	}
 	return (dst);
 }
 
