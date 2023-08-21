c1e18add6197ba998d3ebdd504ace382bda5244e
egdit <105239564+egdit@users.noreply.github.com>
2023-08-21 15:51:35 +0300

    Add files via upload

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,74 @@
+#include "get_next_line.h"
+
+int	ft_index(const char *s, char c)
+{
+	unsigned int	i;
+
+	if (!s)
+		return (-1);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	if (s[i] == c)
+		return (i);
+	return (-1);
+}
+
+unsigned int	ft_strlenn(const char *str)
+{
+	unsigned int	num;
+
+	num = 0;
+	while (str[num] != '\0')
+		num++;
+	return (num);
+}
+
+char	*ft_strdupp(const char *s1)
+{
+	unsigned int	size;
+	char			*ptr;
+	int				i;
+
+	size = ft_strlenn(s1);
+	ptr = malloc((size + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strjoinn(char *s1, char const *s2)
+{
+	char	*string;
+	size_t	i;
+	size_t	x;
+
+	if (!s1)
+		s1 = ft_strdupp("");
+	string = malloc(sizeof(char) * (ft_strlenn(s1) + ft_strlenn(s2) + 1));
+	if (!string)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		string[i] = s1[i];
+		i++;
+	}
+	x = 0;
+	while (s2[x])
+		string[i++] = s2[x++];
+	string[i] = 0;
+	free(s1);
+	return (string);
+}
